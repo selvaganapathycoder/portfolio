389300b985aef78dc914d73b41abd1bfce36095f
selvaganapathy <selvaganapathy172001@gmail.com>
2025-05-07 17:00:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function checkResume() {
+    alert("Redirecting to Resume...");
+    // window.location.href = 'resume.pdf'; // Uncomment to redirect
+  }
+
+  function viewCertificate(imageSrc) {
+    // Open the certificate image in a new tab
+    window.open(imageSrc, '_blank');
+  }
